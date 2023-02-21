5a6364cffc3ef594cef1e9aeeb38675b08d7021f
Joe McLean <joe@Joes-MacBook-Pro.local>
2023-02-21 16:19:44 +0100

    Pan audio based on widget position

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as Tone from 'tone';
 
+
+const panner = new Tone.Panner().toDestination();
+
 const synth1 = new Tone.Synth().toDestination();
-const synth2 = new Tone.Synth().toDestination();
+const synth2 = new Tone.Synth().connect(panner);
 const synth3 = new Tone.Synth().toDestination();
 const synth4 = new Tone.Synth().toDestination();
 
@@ -18,12 +21,20 @@ async function init() {
     synth1.triggerRelease(now + 0.1);
   });
 
-  miro.board.ui.on('selection:update', (event)=> {
+  miro.board.ui.on('selection:update', async (event)=> {
     console.log("Selection update");
     console.log(event);
-    if (event.items.length > 0 && event.items[0].type != "connector") {
+    const viewport = await miro.board.viewport.get();
+    if (event.items.length > 0 && event.items[0].type == "sticky_note") {
       console.log("x: " + event.items[0].x);
       console.log("y: " + event.items[0].y);
+      const viewportOrigin = viewport.x + (viewport.width / 2);
+      const widgetOrigin = event.items[0].x + (event.items[0].width / 2);
+      const xOffset = viewportOrigin - widgetOrigin;
+      var panRatio =-1 * (xOffset * 2) / viewport.width;
+      panRatio = Math.min(Math.max(panRatio, -1), 1);
+      console.log(panRatio);
+      panner.pan.rampTo(panRatio, 0.001);
     }
     const now = Tone.now();
     synth2.triggerAttack("G1", now);
