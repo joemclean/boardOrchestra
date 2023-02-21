d88937990abe62bcd4b74cbca95b453b8b95978d
Joe McLean <joe@Joes-MacBook-Pro.local>
2023-02-21 14:49:39 +0100

    Read current widget position

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const synth2 = new Tone.Synth().toDestination();
 const synth3 = new Tone.Synth().toDestination();
 const synth4 = new Tone.Synth().toDestination();
 
-
 async function init() {
   miro.board.ui.on('icon:click', async () => {
     await miro.board.ui.openPanel({url: 'app.html'});
@@ -20,6 +19,10 @@ async function init() {
 
   miro.board.ui.on('selection:update', (event)=> {
     console.log(event);
+    if (event.items.length > 0 && event.items[0].type != "connector") {
+      console.log("x: " + event.items[0].x);
+      console.log("y: " + event.items[0].y);
+    }
     const now = Tone.now();
     synth2.triggerAttack("G1", now);
     synth2.triggerRelease(now + 0.1);
