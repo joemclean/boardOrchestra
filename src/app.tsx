68f78f1219734a54d3f00fa199b31a9668b24ff1
Joe McLean <joe@Joes-MBP.fritz.box>
2023-02-22 23:09:44 +0100

    Sort of functional start button

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,11 @@ import {createRoot} from 'react-dom/client';
 
 import * as Tone from 'tone';
 
+import { startSequencer, stopSequencer } from './orchestra';
+
 async function startOrchestra() {
   await Tone.start();
+  Tone.Transport.start();
 	console.log('audio is ready');
 }
 
@@ -20,11 +23,25 @@ const App: React.FC = () => {
         >
           Start Orchestra
         </a>
+        <a
+          className="button button-primary"
+          href=""
+          onClick={startSequencer}
+        >
+          Play
+        </a>
+        <a
+          className="button button-primary"
+          href=""
+          onClick={stopSequencer}
+        >
+          Stop
+        </a>
       </div>
     </div>
   );
 };
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container!);
 root.render(<App />);
