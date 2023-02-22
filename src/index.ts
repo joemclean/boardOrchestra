c5c386eae93a143c4e0b9dd373f69d715d620e34
Joe McLean <joe@Joes-MacBook-Pro.local>
2023-02-22 10:55:31 +0100

    Code reorganization, pitch up notes.

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,81 @@ const hihat2 = new Tone.MetalSynth({
 
 drumGain.gain.value = 0.1;
 
+
+// create a new Tone.js Transport object
+const transport = Tone.Transport;
+
+// set the transport time signature to 4/4
+transport.timeSignature = [4, 4];
+
+let iterator = 0;
+
+// create a new Tone.js Sequence object
+const sequence = new Tone.Sequence((time, note) => {
+  
+  // check if this is the 4th beat of the measure
+  if (iterator % 8 === 0) {
+    // play the kick drum sound
+    kick.triggerAttackRelease("C1", "8n", time);
+  } else if ((iterator+4) % 8 === 0) {
+    hihat.triggerAttackRelease("C1", "8n", time);
+  } else {
+    // this function will be called on every other sixteenth note
+    hihat2.triggerAttackRelease("C1", "8n", time);
+  }
+
+  if (iterator % 8 === 0) {
+    if (note1scheduler == 1) {
+      synth1.triggerAttackRelease("C4", "8n", time);
+      note1scheduler = 0;
+      console.log("note1");
+    };
+  
+    if (note2scheduler == 1) {
+      synth2.triggerAttackRelease("G2", "8n", time);
+      note2scheduler = 0;
+      console.log("note2");
+    };
+  
+    if (note3scheduler == 1) {
+      synth3.triggerAttackRelease("G4", "8n", time);
+      note3scheduler = 0;
+      console.log("note3");
+    };
+  
+    if (note4scheduler == 1) {
+      synth4.triggerAttackRelease("C3", "8n", time);
+      synth4.triggerRelease(time + 0.1);
+      note4scheduler = 0;
+      console.log("note4");
+    };
+    
+  }
+
+  iterator++;
+}, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "16n");
+
+// start the transport and the sequence
+transport.start();
+sequence.start();
+
+
+function calculateStereoLocation(viewport: Rect, widget: Rect): number {
+  console.log("x: " + widget.x);
+  console.log("y: " + widget.y);
+  const viewportOrigin = viewport.x + (viewport.width / 2);
+  const widgetOrigin = widget.x + (widget.width / 2);
+  const xOffset = viewportOrigin - widgetOrigin;
+  var panRatio =-1 * (xOffset * 2) / viewport.width;
+  panRatio = Math.min(Math.max(panRatio, -1), 1);
+  console.log(panRatio);
+  return panRatio;
+}
+
+function setPan(panner: Tone.Panner, panRatio: number){
+  panner.pan.rampTo(panRatio, 0.001);
+}
+
 async function init() {
   miro.board.ui.on('icon:click', async () => {
     await miro.board.ui.openPanel({url: 'app.html'});
@@ -97,79 +172,5 @@ async function init() {
     }
   });
 }
-
-function calculateStereoLocation(viewport: Rect, widget: Rect): number {
-  console.log("x: " + widget.x);
-  console.log("y: " + widget.y);
-  const viewportOrigin = viewport.x + (viewport.width / 2);
-  const widgetOrigin = widget.x + (widget.width / 2);
-  const xOffset = viewportOrigin - widgetOrigin;
-  var panRatio =-1 * (xOffset * 2) / viewport.width;
-  panRatio = Math.min(Math.max(panRatio, -1), 1);
-  console.log(panRatio);
-  return panRatio;
-}
-
-function setPan(panner: Tone.Panner, panRatio: number){
-  panner.pan.rampTo(panRatio, 0.001);
-}
-
-// create a new Tone.js Transport object
-const transport = Tone.Transport;
-
-// set the transport time signature to 4/4
-transport.timeSignature = [4, 4];
-
-
-// create a new Tone.js Sequence object
-let iterator = 0;
-
-const sequence = new Tone.Sequence((time, note) => {
-  
-  // check if this is the 4th beat of the measure
-  if (iterator % 8 === 0) {
-    // play the kick drum sound
-    kick.triggerAttackRelease("C1", "8n", time);
-  } else if ((iterator+4) % 8 === 0) {
-    hihat.triggerAttackRelease("C1", "8n", time);
-  } else {
-    // this function will be called on every other sixteenth note
-    hihat2.triggerAttackRelease("C1", "8n", time);
-  }
-
-  if (iterator % 8 === 0) {
-    if (note1scheduler == 1) {
-      synth1.triggerAttackRelease("C3", "8n", time);
-      note1scheduler = 0;
-      console.log("note1");
-    };
-  
-    if (note2scheduler == 1) {
-      synth2.triggerAttackRelease("G1", "8n", time);
-      note2scheduler = 0;
-      console.log("note2");
-    };
-  
-    if (note3scheduler == 1) {
-      synth3.triggerAttackRelease("G3", "8n", time);
-      note3scheduler = 0;
-      console.log("note3");
-    };
-  
-    if (note4scheduler == 1) {
-      synth4.triggerAttackRelease("C2", "8n", time);
-      synth4.triggerRelease(time + 0.1);
-      note4scheduler = 0;
-      console.log("note4");
-    };
-    
-  }
-
-  iterator++;
-}, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "16n");
-
-// start the transport and the sequence
-transport.start();
-sequence.start();
 
 init();
