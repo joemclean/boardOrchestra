74a4859ba3e83bcf48291d408cb574dd28f583fb
Joe McLean <joe@Joes-MacBook-Pro.local>
2023-02-23 13:42:05 +0100

    Change app height

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 async function init() {
   miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
+    await miro.board.ui.openPanel({url: 'app.html', height: 200});
   })};
 
 init();
