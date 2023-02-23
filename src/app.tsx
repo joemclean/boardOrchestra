e2ef19adff6e4006695145a86738635ceeb9a4bf
Joe McLean <joe@Joes-MacBook-Pro.local>
2023-02-23 16:15:31 +0100

    Stable state

--- src/app.tsx
+++ src/app.tsx
@@ -17,42 +17,60 @@ const App: React.FC = () => {
   );
 };
 
-miro.board.ui.on('experimental:items:update', async (event)=> {
-  const viewport = await miro.board.viewport.get();
-  if (event.items.length > 0 && event.items[0].type == "sticky_note") {
-    const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
-    orchestra.updateWidgetScheduler.synthVoice.setPan(panRatio);
-  }
-  if (Math.random() > 0.5) {
-    orchestra.updateWidgetScheduler.scheduleNoteToPlay("E5");
-    orchestra.updateWidgetScheduler.scheduleNoteToPlay("A5");
-  } else {
-    orchestra.updateWidgetScheduler.scheduleNoteToPlay("A5");
-    orchestra.updateWidgetScheduler.scheduleNoteToPlay("G5");
-  }
-});
-
-miro.board.ui.on('selection:update', async (event)=> {
-  const viewport = await miro.board.viewport.get();
-  if (event.items.length > 0 && event.items[0].type == "sticky_note") {
-    const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
-    orchestra.updateSelectionScheduler.synthVoice.setPan(panRatio);
-  }
-  if (event.items.length > 0) {
-    orchestra.widgetSelected = true;
-    // more notes for multiselect
-    if (event.items.length > 1) {
-      orchestra.updateSelectionScheduler.scheduleNoteToPlay("C4");
-      orchestra.updateSelectionScheduler.scheduleNoteToPlay("E4");
+  miro.board.ui.on('experimental:items:update', async (event)=> {
+    const viewport = await miro.board.viewport.get();
+    if (event.items.length > 0 && event.items[0].type == "sticky_note") {
+      const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
+      orchestra.updateWidgetScheduler.synthVoice.setPan(panRatio);
     }
-    orchestra.updateSelectionScheduler.scheduleNoteToPlay("G4");
-  } else {
-    orchestra.widgetSelected = false;
-    orchestra.updateSelectionScheduler.scheduleNoteToPlay("G4");
-    orchestra.updateSelectionScheduler.scheduleNoteToPlay("E4");
-    orchestra.updateSelectionScheduler.scheduleNoteToPlay("C4");
-  }
-});
+    if (Math.random() > 0.5) {
+      orchestra.updateWidgetScheduler.scheduleNoteToPlay("E5");
+      orchestra.updateWidgetScheduler.scheduleNoteToPlay("A5");
+    } else {
+      orchestra.updateWidgetScheduler.scheduleNoteToPlay("A5");
+      orchestra.updateWidgetScheduler.scheduleNoteToPlay("G5");
+    }
+  });
+
+  miro.board.ui.on('selection:update', async (event)=> {
+    const viewport = await miro.board.viewport.get();
+    if (event.items.length > 0 && event.items[0].type == "sticky_note") {
+      const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
+      orchestra.updateSelectionScheduler.synthVoice.setPan(panRatio);
+    }
+    if (event.items.length > 0) {
+      orchestra.widgetSelected = true;
+      // more notes for multiselect
+      if (event.items.length > 1) {
+        orchestra.updateSelectionScheduler.scheduleNoteToPlay("C3");
+        orchestra.updateSelectionScheduler.scheduleNoteToPlay("E3");
+      }
+      orchestra.updateSelectionScheduler.scheduleNoteToPlay("G3");
+    } else {
+      orchestra.widgetSelected = false;
+      orchestra.updateSelectionScheduler.scheduleNoteToPlay("G3");
+      orchestra.updateSelectionScheduler.scheduleNoteToPlay("E3");
+      orchestra.updateSelectionScheduler.scheduleNoteToPlay("C3");
+    }
+  });
+
+  miro.board.ui.on('items:create', async (event)=> {
+    const viewport = await miro.board.viewport.get();
+    if (event.items.length > 0 && event.items[0].type == "sticky_note") {
+      const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
+      orchestra.createWidgetScheduler.synthVoice.setPan(panRatio);
+    }
+    orchestra.createWidgetScheduler.scheduleNoteToPlay("G2");
+  });
+
+  miro.board.ui.on('items:delete', async (event)=> {
+    const viewport = await miro.board.viewport.get();
+    if (event.items.length > 0 && event.items[0].type == "sticky_note") {
+      const panRatio = orchestra.calculateStereoLocation(viewport, event.items[0]);
+      orchestra.deleteWidgetScheduler.synthVoice.setPan(panRatio);
+    }
+    orchestra.deleteWidgetScheduler.scheduleNoteToPlay("C2");
+  });
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
